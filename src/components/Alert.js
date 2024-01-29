df1b8707cce6ab2b4247427a1392d10c6c11e086
water-p <psi94082@gmail.com>
2024-01-29 22:14:32 +0900

    first commit

---
+++ src/components/Alert.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Alert.css';
+
+export const Alert = ({type, text}) => {
+    return (
+        <div className={`alert alert-${type}`}>{text}</div>
+    )
+}
+
+export default Alert
---
+++ src/components/ExpenseList.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import "./ExpenseList.css";
+import ExpenseItem from './ExpenseItem';
+import {MdDelete} from 'react-icons/md';
+
+const ExpenseList = ({handleDelete, expense,handleEdit,clearItems}) => {
+  return (
+    <>
+      <ul className='list'>
+        {/*Expense Item */}
+        {expense.map(expense=>{
+          return (
+            <ExpenseItem 
+              expense={expense}
+              key={expense.id}
+              handleDelete={handleDelete}
+              handleEdit={handleEdit}
+            />
+          )
+        })}
+      </ul>
+      {expense.length> 0 && (
+        <button className='btn' onClick={clearItems}>
+        목록 지우기
+        <MdDelete className='btn-icon'/>
+      </button>
+      )}
+      
+    </>
+  )
+}
+
+export default ExpenseList
